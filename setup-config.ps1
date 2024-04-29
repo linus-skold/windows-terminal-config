8ea8e8bf47af9621023cdfc9781f14b405ad8cee
Linus <contact@linusskold.com>
2024-04-29 22:28:24 +0200

    adding oh my posh to the path and creating theme dir

--- setup-config.ps1
+++ setup-config.ps1
@@ -1,10 +1,8 @@
 #Requires -RunAsAdministrator
 
-
 param(
     [bool]$Uninstall = $false
 )
-
 
 # check if we\re running as admin
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -41,42 +39,43 @@ if($Uninstall) {
             return
         }
     }
-    
+
     # Install packages
     "Starting to install packages"
     choco install -y git
     choco install -y vscode-insiders
+
     choco install -y oh-my-posh
-    
-    
-    $cascadiaCodeInstall = oh-my-posh font install "CascadiaCode" 
-    $firaCodeInstall = oh-my-posh font install "FiraCode" 
-    
+    $env:Path += ";$env:LOCALAPPDATA\Programs\oh-my-posh\bin"
+
+    oh-my-posh font install "CascadiaCode" 
+    oh-my-posh font install "FiraCode" 
+
     # Set Windows Terminal settings
     $settingsFilePath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
     $colorScheme = "One Half Dark"
-    
+
     # Update Windows Terminal settings
     $settings = Get-Content $settingsFilePath | ConvertFrom-Json
-    
+
     $settings.profiles.defaults.colorScheme = $colorScheme
     $settings.profiles.defaults.font.face = "CaskaydiaMono Nerd Font Mono"
     $settings.profiles.defaults.opacity = 80
-    $settings.profiles.defaults.useAcrylic = true
-    
+    $settings.profiles.defaults.useAcrylic = $true
+
     $updatedSettings = $settings | ConvertTo-Json -Depth 50
     $updatedSettings | Set-Content $settingsFilePath
-    
+
     # Install Nushell
     choco install -y nushell
-    
+
     # Download & Install theme for Nushell
     $theme = "catppuccin_frappe"
     $themeURL = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/$theme.omp.json"
     $themePath = "$env:APPDATA\nushell\themes\$theme.omp.json"
+    New-Item -ItemType Directory -Force -Path "$env:APPDATA\nushell\themes"
     Invoke-WebRequest -Uri "$themeURL" -OutFile "$themePath"
-    
-    
+
     # setup an oh-my-posh config for nushell
     oh-my-posh init nu --config $themePath
     # add the oh-my-posh config to the nushell config
